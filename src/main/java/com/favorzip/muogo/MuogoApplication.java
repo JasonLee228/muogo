e502e21ec300eb412938982a7a9466b238001ba6
changeun.lee <loveck228@gmail.com>
2023-03-05 16:33:05 +0900

    230305 최초 커밋

---
+++ src/main/java/com/favorzip/muogo/MuogoApplication.java
@@ -0,0 +1,13 @@
+package com.favorzip.muogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuogoApplication.class, args);
+    }
+
+}
